ff0e87831c16e992e3638de974f2585d6392022b
Nilay Jain <31623181+nilayjain12@users.noreply.github.com>
2024-11-05 21:03:43 -0800

    V1.0

---
+++ backend/config.py
@@ -0,0 +1,9 @@
+import os
+
+# Setting BASE_DIR of the Project
+base_path = r'C:\Users\nilay\OneDrive - Cal State Fullerton (1)\Desktop\NILAY-TO-JOB-DATA\SPRING 2024\Projects\Gmail-JobEmail-Classifier'
+BASE_DIR = os.path.abspath(base_path)
+
+# Setting the Data directory of the Project
+data_path = r'C:\Users\nilay\OneDrive - Cal State Fullerton (1)\Desktop\NILAY-TO-JOB-DATA\SPRING 2024\Projects\Gmail-JobEmail-Classifier\backend\data'
+DATADIR = os.path.abspath(data_path)
