03c0adcde09bb0ad946c009f88067f04cc284d4c
Nilay Jain <31623181+nilayjain12@users.noreply.github.com>
2024-11-05 23:00:11 -0800

    V1.0-testing leaniar regression model

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ import os
 
 # Setting BASE_DIR of the Project
 base_path = r'C:\Users\nilay\OneDrive - Cal State Fullerton (1)\Desktop\NILAY-TO-JOB-DATA\SPRING 2024\Projects\Gmail-JobEmail-Classifier'
-BASE_DIR = os.path.abspath(base_path)
+BASEDIR = os.path.abspath(base_path)
 
 # Setting the Data directory of the Project
 data_path = r'C:\Users\nilay\OneDrive - Cal State Fullerton (1)\Desktop\NILAY-TO-JOB-DATA\SPRING 2024\Projects\Gmail-JobEmail-Classifier\backend\data'
